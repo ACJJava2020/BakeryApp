c023ea09e90731a645e57834e7ef649d3eaa045f
GBTC440019UR <GBTC440019UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-05 11:50:40 -0500

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+    public static void main (String [] args) {
+        System.out.println("Welcome to the Brookley's Better Bakery App!" +
+                "\nOur products contain: [peanuts, gluten, soy, dairy] ");
+        System.out.println();
+        System.out.print("To search for foods that you can eat, please enter the name of the diet restriction:" +
+                "\nTo see a list of all our foods, please type \"all\". ");
+
+    }
 }
